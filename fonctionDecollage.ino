7c46b38df0f7dbaac36b5d5492473671173a9c49
AubinMercier <79213491+AubinMercier@users.noreply.github.com>
2021-03-06 15:02:29 +0100

    Update fonctionDecollage.ino

--- fonctionDecollage.ino
+++ fonctionDecollage.ino
@@ -1,5 +1,5 @@
 double normeAcc = sqrt(pow(d.acceleration[0],2)+pow(d.acceleration[1],2)+pow(d.acceleration[2],2));
 double normeDecollage = ; //insérer norme
 if(normeAcc>normeDecollage){
-  State=DECOLLAGE
+  State = DECOLLAGE;
 }
