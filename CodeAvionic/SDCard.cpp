976163de997220d535cbdd8fc3c446db370a1f90
AubinMercier <79213491+AubinMercier@users.noreply.github.com>
2021-03-14 15:24:54 +0100

    Update SDCard.cpp

--- CodeAvionic/SDCard.cpp
+++ CodeAvionic/SDCard.cpp
@@ -1,24 +1,36 @@
 #include <Arduino.h>
 #include "SDCard.h"
+#include <SD.h>
+#include <SPI.h>
+
+const int chipSelect = BUILTIN_SDCARD;
+
 void SDCard::setupSDCard(){
-  Serial.print("Initializing SD card...");
+  //Serial.print("Initializing SD card...");
   
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+    //Serial.println("Card failed, or not present");
     // don't do anything more:
+    tone(22,1000,20000);//buzzer si problème d'initialisation
     return;
   }
-  Serial.println("card initialized.");
+  //Serial.println("card initialized.");
   }
+
 void SDCard::createFile(){
-  dataFile = SD.open("datalog.txt", FILE_WRITE);
+  File dataFile = SD.open("data.csv", FILE_WRITE);
+  String dataString = "Altitude,OrientationX,OrientationY,OrientationZ,AccelerationX,AccelerationY,AccelerationZ,Temperature,Battery,";
+  if(dataFile){
+    dataFile.println(dataString);
   }
+  }
+
 void SDCard::printData (Data * d){
   String dataString = d->altitude + ",";
-  for(int i=0 ; i<3 ; ++i)        //request acc data from MPU
+  for(int i=0 ; i<3 ; ++i)        //request gyr data from MPU
     dataString += String(d->orientation[i])+ ",";
-  for(int i=0 ; i<3 ; ++i)        //request gyr data
+  for(int i=0 ; i<3 ; ++i)        //request acc data
     dataString += String(d->acceleration[i])+ ",";
   dataString = d->temperature + ",";  
   dataString = d->battery_level + ","; 
@@ -26,13 +38,14 @@ void SDCard::printData (Data * d){
     dataFile.println(dataString);
     
     // print to the serial port too:
-    Serial.println(dataString);
+    //Serial.println(dataString);
   }  
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
+    //Serial.println("error opening datalog.txt");
   }  
 }
+
 void SDCard::closeFile(){
   dataFile.close();
   }
